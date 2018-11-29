1dd395e498fc4d0a93ac1f343ab632c081c81de9
蘭雅sRGB <hongwenjun@gmail.com>
2018-11-29 09:08:05 +0800

    Update wgmtu.sh

--- Wireguard/wgmtu.sh
+++ Wireguard/wgmtu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo -e "WireGuard 修复MTU过低，导致网速下降，修改WG服务器端MTU值，默认值 MTU = 1420"
-read -p "请输入数字: " num
+echo -e "WireGuard 客户端可以MTU参数自动，请把客户段的MTU行注释掉。"
+read -p "请输入数字(1200--1500): " num
 
 if [[ ${num} -ge 1200 ]] && [[ ${num} -le 1500 ]]; then
    mtu=$num
