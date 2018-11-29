8f8a493eca8fa84f241a6e9292ffad8bebfae392
蘭雅sRGB <hongwenjun@gmail.com>
2018-11-29 09:23:31 +0800

    Update wgmtu.sh

--- Wireguard/wgmtu.sh
+++ Wireguard/wgmtu.sh
@@ -6,7 +6,7 @@ Green="\033[32m"  && Red="\033[31m" && GreenBG="\033[42;37m" && RedBG="\033[41;3
 
 echo -e "${GreenBG}WireGuard 修改服务器端MTU值，最大效率加大网速，默认值 MTU = 1420 "
 echo -e "WireGuard 客户端可以MTU参数自动，请修改电脑客户端TunSafe配置把MTU行注释掉。${Font}"
-read -p "请输入数字{RedBG}(1200--1500):${Font} " num
+read -p "请输入数字(1200--1500): " num
 
 if [[ ${num} -ge 1200 ]] && [[ ${num} -le 1500 ]]; then
    mtu=$num
