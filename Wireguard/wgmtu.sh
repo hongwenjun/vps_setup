2829919804a3c177dfba94c13d145acd449bc740
蘭雅sRGB <hongwenjun@gmail.com>
2018-12-02 23:29:50 +0800

    Update wgmtu.sh

--- Wireguard/wgmtu.sh
+++ Wireguard/wgmtu.sh
@@ -8,20 +8,20 @@ setmtu(){
     echo -e "${GreenBG}WireGuard 修改服务器端MTU值，最大效率加大网速，默认值 MTU = 1420 "
     echo -e "WireGuard 客户端可以MTU参数自动，请修改电脑客户端TunSafe配置把MTU行注释掉。${Font}"
     read -p "请输入数字(1200--1500): " num
-    
+
     if [[ ${num} -ge 1200 ]] && [[ ${num} -le 1500 ]]; then
        mtu=$num
     else
-       mtu=1420	
+       mtu=1420
     fi
-    	
+
     wg-quick down wg0
     sed -i "s/MTU = .*$/MTU = ${mtu}/g"  /etc/wireguard/wg0.conf
-    
+
     wg-quick up wg0
-    
-    echo -e "${RedBG}服务器端MTU值已经修改!${Font}"   
-    
+
+    echo -e "${RedBG}服务器端MTU值已经修改!${Font}"
+
 }
 
 
@@ -29,49 +29,63 @@ setmtu(){
 setport(){
     echo -e "${GreenBG}修改 WireGuard 服务器端端口号，客户端要自行修改${Font}"
     read -p "请输入数字(100--60000): " num
-    
+
     if [[ ${num} -ge 100 ]] && [[ ${num} -le 60000 ]]; then
        port=$num
        wg-quick down wg0
-       sed -i "s/ListenPort = .*$/ListenPort = ${port}/g"  /etc/wireguard/wg0.conf  
+       sed -i "s/ListenPort = .*$/ListenPort = ${port}/g"  /etc/wireguard/wg0.conf
        wg-quick up wg0
-       echo -e "${RedBG}端口号已经修改!${Font}"   
+       echo -e "${RedBG}端口号已经修改!${Font}"
     else
-       echo -e "${RedBG}没有修改端口号!${Font}"             
+       echo -e "${RedBG}没有修改端口号!${Font}"
     fi
 
 }
 
-wgconf(){
-bash wg5
+wgconf()
+{
+    bash wg5
+}
+
+udp2raw()
+{
+    wget -qO- https://git.io/fpKnF | bash
+    echo -e "${RedBG}:: WireGuard 使用 Udp2Raw 需要把 MTU 设置成1200-1300  ${Font}"
+    echo -e "${GreenBG}:: 您可以在本脚本基础上，修改成加速脚本！... 你懂的！${Font}"
 }
 
 # 设置菜单
 start_menu(){
-    echo -e "${Green}1. 显示客户端配置文本，临时网页下载客户端"
-    echo -e "2. 修改 WireGuard 服务器端 MTU 值"
-    echo -e "3. 修改 WireGuard 端口号"
-    echo -e "4. 退出设置${Font}"
+    echo -e "${RedBG}   一键安装 WireGuard 脚本 For Debian_9 Ubuntu Centos_7   ${Font}"
+    echo -e "${GreenBG}    开源项目：https://github.com/hongwenjun/vps_setup    ${Font}"
+    echo -e "${Green}>  1. 显示客户端配置文本，临时网页下载客户端"
+    echo -e ">  2. 修改 WireGuard 服务器端 MTU 值"
+    echo -e ">  3. 修改 WireGuard 端口号"
+    echo -e ">  4. 安装Udp2Raw服务TCP伪装 WireGuard 服务端设置"
+    echo -e ">  5. 退出设置${Font}"
     echo
-    read -p "请输入数字(1-4):" num
+    read -p "请输入数字(1-5):" num
     case "$num" in
         1)
-	wgconf
-	;;
-    	2)
-	setmtu
-	;;
-	3)
-	setport
-	;;
-	4)
-	exit 1
-	;;
-	*)
-	clear
-	echo "请输入正确数字"
-	;;
-    esac
+        wgconf
+        ;;
+        2)
+        setmtu
+        ;;
+        3)
+        setport
+        ;;
+        4)
+        udp2raw
+        ;;
+        5)
+        exit 1
+        ;;
+        *)
+        echo
+        echo "请输入正确数字"
+        ;;
+        esac
 }
 
 start_menu
