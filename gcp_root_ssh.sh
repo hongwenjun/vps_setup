0aea8b016668c313df9201aa1bf4a37ff9292a9b
蘭雅sRGB <hongwenjun@gmail.com>
2018-12-11 08:49:52 +0800

    Update gcp_root_ssh.sh

--- gcp_root_ssh.sh
+++ gcp_root_ssh.sh
@@ -5,12 +5,12 @@
 #  GCP一键启用root帐号命令
 #  wget -qO- git.io/fpQWf | bash
 
-# 修改root 密码
-passwd
-
 # GCP 启用root密码登陆
 sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g"   /etc/ssh/sshd_config
 sed -i "s/PasswordAuthentication.*/PasswordAuthentication yes/g"   /etc/ssh/sshd_config
 
 # 重启ssh服务
 systemctl restart ssh
+
+# 修改root 密码
+passwd
