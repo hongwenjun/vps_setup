15d4f28716e82ea04d2048dd86948614880be0c1
蘭雅sRGB <hongwenjun@gmail.com>
2018-12-17 10:03:04 +0800

    Update debian_gnome.sh

--- debian_gnome.sh
+++ debian_gnome.sh
@@ -5,20 +5,20 @@
 
 # Debian安装Gnome桌面脚本
 apt update
-apt install x-window-system-core
-apt install gnome-core
+apt install -y x-window-system-core
+apt install -y gnome-core
 
 # 安装中文显示(建议安装）：
 # 让gnome面板、菜单显示中文 
-apt-get install language-pack-gnome-zh
-apt-get install language-pack-gnome-zh-base 
+apt-get install -y language-pack-gnome-zh
+apt-get install -y language-pack-gnome-zh-base 
 # 中文语言包 
-apt-get install language-pack-zh
-apt-get install language-pack-zh-base
+apt-get install -y language-pack-zh
+apt-get install -y language-pack-zh-base
 # 中文语言支持
-apt-get install language-support-zh
+apt-get install -y language-support-zh
 # scim中文输入法平台
-apt-get install scim
+apt-get install -y scim
 
 # 进入图形界面： startx
 # 退出图形桌面： ctrl + alt + backspace
