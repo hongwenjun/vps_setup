94d9d48dcd6a871af97b88b2f089868b371ba3d8
蘭雅sRGB <hongwenjun@gmail.com>
2019-03-07 20:40:50 +0800

    Create vps.sh

---
+++ vps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 一键安装wireguard 脚本
+wget -qO- git.io/fptwc | bash
+
+# 一键安装shadowsocks-libev脚本
+wget -qO- git.io/fhExJ | bash
+
+# V2Ray官方一键脚本
+bash <(curl -L -s https://install.direct/go.sh)
