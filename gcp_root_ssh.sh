7014d5932988c8a0d6c86eb19335c12726eb1a1f
蘭雅sRGB <hongwenjun@gmail.com>
2020-11-05 10:16:11 +0800

    Update gcp_root_ssh.sh

--- gcp_root_ssh.sh
+++ gcp_root_ssh.sh
@@ -7,6 +7,7 @@
 
 # GCP 启用root密码登陆
 sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g"   /etc/ssh/sshd_config
+sed -i "s/#PermitRootLogin.*/PermitRootLogin yes/g"   /etc/ssh/sshd_config
 sed -i "s/PasswordAuthentication.*/PasswordAuthentication yes/g"   /etc/ssh/sshd_config
 
 # 重启ssh服务
