aadd707f3001c041b3a2aba00b7fbf5189e56d9f
hongwenjun <hongwenjun@gmail.com>
2021-05-27 20:52:18 +0800

    更新 autopt.sh 一键脚本和说明

--- pt/README.md
+++ pt/README.md
@@ -1,13 +1,72 @@
-![QB.png][1]
+![QB.png](https://262235.xyz/usr/uploads/2021/05/3140493615.png)
 
 ### 1)  请先看大神博客  自动PT工具参数调优
 
 https://www.taterli.com/7785/
 
-### 1.1) 准备接下来写 一键脚本 autopt.sh 方便配置管理 goseeder.conf 
-- 如果参数很容易搞错，浪费了半天，重新制作容器才成功
-- 把工具制作成Docker容器，原则上群晖NAS也可以方便使用
+### 1.1) 自动PT autopt.sh 一键脚本  方便配置测试 goseeder.conf
+- 短网址: https://git.io/autopt.sh
+- 把工具制作成Docker容器，测试支持群晖NAS，可以使用
 
+```
+wget git.io/autopt.sh && bash autopt.sh
+
+# 群晖NAS使用如果不能访问外网 使用命令
+wget 262235.xyz/autopt.sh && bash autopt.sh
+```
+![autopt.png][1]
+
+![conf.png](https://262235.xyz/usr/uploads/2021/05/3539043976.png)
+
+###  复制示例到Json工具网站编辑，保存上传 /root/goseeder.conf
+
+```
+{
+"dbserver": "mongodb+srv://pt:passwd@cluster0.XXXX.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+
+"node": [
+
+{
+"source": "pt.msg.vg",
+"passkey": "123456789-PASSKEY-PASSKEY-ABCDEF",
+"limit": 10,
+"enable": true,
+"rule": {
+    "seeder_time": 0,
+    "seeder_ratio": 0,
+    "speed_limit": 8.0
+}
+}
+
+
+],
+
+
+"server": [
+
+{
+"endpoint": "http://192.168.1.222:8080",
+"username": "admin",
+"password": "adminadmin",
+"remark": "DISKSTATION",
+"enable": true,
+"rule": {
+"concurrent_download": 1,
+"disk_threshold": 10.0,
+"disk_overcommit": false,
+"max_speed": 30.00,
+"min_alivetime": 3600,
+"max_alivetime": 86400,
+"min_tasksize": 0.0,
+"max_tasksize": 50.0,
+"max_disklatency": 10000
+}
+}
+
+]
+}
+
+```
 
 ### 2) 运行 goseeder 自动PT
 
@@ -19,6 +78,8 @@ https://www.taterli.com/7785/
 ### 3) 查看QB服务器设置是否正常
 
     docker  exec  -it autopt  gostat
+
+![](https://262235.xyz/usr/uploads/2021/05/962808404.png)
 
 ### 4) 检查RSS推送信息是否正确
 
@@ -32,14 +93,16 @@ https://www.taterli.com/7785/
       -v /opt/portainer_data:/data        \
       portainer/portainer
 
-![123.png][2]
-  
+![123.png](https://262235.xyz/usr/uploads/2021/05/354698449.png)
+
 ### 6) 使用Portainer 查看和修改比较方便
 
 - 编辑  /root/goseeder.conf  后使用网页管理重启，或者命令
 
     docker restart autopt
 
-
-  [1]: https://262235.xyz/usr/uploads/2021/05/3140493615.png
-  [2]: https://262235.xyz/usr/uploads/2021/05/354698449.png
+  [1]: https://262235.xyz/usr/uploads/2021/05/2184208616.png
+  [2]: https://262235.xyz/usr/uploads/2021/05/3539043976.png
+  [3]: https://262235.xyz/usr/uploads/2021/05/354698449.png
+  [4]: https://262235.xyz/usr/uploads/2021/05/3539043976.png
+  [5]: https://262235.xyz/usr/uploads/2021/05/962808404.png
--- pt/autopt.sh
+++ pt/autopt.sh
@@ -6,19 +6,20 @@ SERVER_IP=172.17.0.1
 PORT=8080
 PASSWORD="adminadmin"
 
-MONGODB_API="mongodb+srv://pt:Pa55w.rd@cluster0.m9ox4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+# 此 Mongodb 数据连接帐号, 只供初次测试配置使用，请至 https://www.mongodb.com/ 注册私人免费帐号
+
+MONGODB_API="pt:Pa55w.rd@cluster0.m9ox4"
 RSS_DOMAIN="RSS.AUTOPT.NET"
 RSS_PASSKEY="123456789-PASSKEY-PASSKEY-ABCDEF"
 
 set_qb_conf()
 {
-	echo -e "${SkyBlue}:: Docker版 qBittorrent Web 默认: ${GreenBG} http://${SERVER_IP}:${PORT} ${SkyBlue} 密码:${RedBG} ${PASSWORD} "
+    echo -e "${SkyBlue}:: Docker版 qBittorrent Web 默认: ${GreenBG} http://${SERVER_IP}:${PORT} ${SkyBlue} 密码:${RedBG} ${PASSWORD} "
     echo -e "${SkyBlue}:: 请按实际信息修改，下面三项按 ${RedBG}<Enter>${SkyBlue} 使用默认值! ${Yellow}"
 
     read -p ":: 1.请输入 qBittorrent Web 服务器IP (172.17.0.1): "  sv_ip
     read -p ":: 2.请输入 qBittorrent Web 服务端口   ( 8080 )  : "  port
     read -p ":: 3.请输入 qBittorrent Web 管理密码 (adminadmin): "  passwd
-
 
     if [[ ! -z "${sv_ip}" ]]; then
         SERVER_IP=${sv_ip}
@@ -30,18 +31,17 @@ set_qb_conf()
         PASSWORD=${passwd}
     fi
 
-	echo -e "${SkyBlue}:: 设置后 qBittorrent Web管理网址: ${GreenBG} http://${SERVER_IP}:${PORT} ${SkyBlue} 密码:${RedBG} ${PASSWORD} "
+    echo -e "${SkyBlue}:: 设置后 qBittorrent Web管理网址: ${GreenBG} http://${SERVER_IP}:${PORT} ${SkyBlue} 密码:${RedBG} ${PASSWORD} "
     echo -e ${Font}
 }
 
 set_passkey_conf()
 {
-	echo -e "${SkyBlue}:: 请提前登陆 PT 网站生成 RSS 订阅地址; 获得${GreenBG} PASSKEY ${SkyBlue}和RSS域名, 键盘按 ${RedBG}<Shift+Insert>${RedBG}${SkyBlue} 粘贴"
+    echo -e "${SkyBlue}:: 请提前登陆 PT 网站生成 RSS 订阅地址; 获得${GreenBG} PASSKEY ${SkyBlue}和RSS域名, 键盘按 ${RedBG}<Shift+Insert>${RedBG}${SkyBlue} 粘贴"
     echo -e "${SkyBlue}:: 请按实际信息修改，按 ${RedBG}<Enter>${SkyBlue} 使用伪值填充! ${Yellow}"
 
     read -p ":: 4.请输入RSS-PASSKEY (123456789-PASSKEY-PASSKEY-ABCDEF) : "  passkey
     read -p ":: 5.请输入RSS 订阅地址的域名 ( pt.msg.vg ): "  domain
-
 
     if [[ ! -z "${domain}" ]]; then
         RSS_DOMAIN=${domain}
@@ -50,7 +50,7 @@ set_passkey_conf()
         RSS_PASSKEY=${passkey}
     fi
 
-	echo -e "${SkyBlue}:: 设置后RSS域名: ${GreenBG} ${RSS_DOMAIN} ${SkyBlue} RSS-PASSKEY: ${RedBG} ${RSS_PASSKEY} "
+    echo -e "${SkyBlue}:: 设置后RSS域名: ${GreenBG} ${RSS_DOMAIN} ${SkyBlue} RSS-PASSKEY: ${RedBG} ${RSS_PASSKEY} "
     echo -e ${Font}
 }
 
@@ -82,7 +82,7 @@ init_config(){
 
     cat <<EOF >/root/goseeder.conf
 {
-"dbserver": "${MONGODB_API}",
+"dbserver": "mongodb+srv://${MONGODB_API}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 "node": [
   {
 "source": "${RSS_DOMAIN}",
@@ -117,32 +117,30 @@ init_config(){
   }
  ]
 }
+
 EOF
 
     echo -e "${SkyBlue}:: 初始化自动PT工具配置文件完成: ${GreenBG} /root/goseeder.conf ${Font}"
     cat /root/goseeder.conf
+    echo -e  "${RedBG}# 此 Mongodb 数据连接帐号, 只供初次测试配置使用 ${SkyBlue} 请至 https://www.mongodb.com/ 注册私人免费帐号"
 }
-
 
 system_def(){
-	Green="\033[32m"  && Red="\033[31m" && GreenBG="\033[42;37m" && RedBG="\033[41;37m"
-	Font="\033[0m"  && Yellow="\033[0;33m" && SkyBlue="\033[0;36m"
+    Green="\033[32m"  && Red="\033[31m" && GreenBG="\033[42;37m" && RedBG="\033[41;37m"
+    Font="\033[0m"  && Yellow="\033[0;33m" && SkyBlue="\033[0;36m"
 }
-
 
 install(){
-docker run -d  --name  autopt  \
-   -v  /root/goseeder.conf:/goseeder.conf  \
-   hongwenjun/autopt
+  docker run -d  --name  autopt  \
+    -v  /root/goseeder.conf:/goseeder.conf  \
+    hongwenjun/autopt
 
-echo -e "${GreenBG}::   autopt镜像:   https://hub.docker.com/r/hongwenjun/autopt ${Font}"
+    echo -e "${GreenBG}::   autopt镜像:   https://hub.docker.com/r/hongwenjun/autopt ${Font}"
 }
-
 
 # 设置菜单
 start_menu(){
-    clear
-    echo -e "${GreenBG}>      开源项目:  https://github.com/hongwenjun/vps_setup    "
+    echo -e "${GreenBG}>     开源项目:  https://github.com/hongwenjun/vps_setup     "
     echo -e "${RedBG}>  自动PT autopt.sh 一键脚本 感谢大神TaterLi创作goseeder工具 ${Font}"
     echo -e "${SkyBlue}>  1. 重置初始化自动PT工具配置文件(首次需运行)"
     echo -e ">  2. 安装 Docker 容器 autopt 镜像"
@@ -159,11 +157,11 @@ start_menu(){
     case "$num" in
         1)
         input_conf
-	start_menu
+        start_menu
         ;;
         2)
         install
-	start_menu
+        start_menu
         ;;
         3)
         docker restart autopt
@@ -186,12 +184,13 @@ start_menu(){
         8)
         docker logs  --tail=100 autopt | grep -e 种子 -e 磁盘
         ;;
-	*)
-	docker logs  --tail=100 autopt | grep -e 种子 -e 磁盘
-	;;
+        *)
+        docker logs  --tail=100 autopt | grep -e 种子 -e 磁盘
+        ;;
         esac
 }
 
+clear
 system_def
 start_menu
 
