8a14bbe7755591cf45966c10be6f77ce9469e749
蘭雅sRGB <hongwenjun@gmail.com>
2021-10-09 12:49:06 +0800

    Update autonet.sh

--- debian/autonet.sh
+++ debian/autonet.sh
@@ -2,7 +2,6 @@
 # autonet.sh  自动修改interfaces 网卡自动加载
 
 lspci | grep Ethernet
-ni=$(ip addr | grep enp | head -n 1 | awk -F ': '  '{print $2}')
+ni=$(ip addr | grep -E en[a-zA-Z0-9] | head -n 1 | awk -F ': '  '{print $2}')
 sed -i "s/enp[a-zA-Z0-9]*/${ni}/g"  /etc/network/interfaces
 ifup $ni
-
